b51d424dbcc57be2366e820bdafd9b38adbdd761
unknown <ricardoarrecisrivera@gmail.com>
2025-04-17 08:19:28 -0600

    Se actualiza 2

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 app.register_blueprint(auth_bp)
 # app.register_blueprint(usuarios_bp)
 
+# Esto solo se ejecuta si corres app.py directamente (en desarrollo)
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
